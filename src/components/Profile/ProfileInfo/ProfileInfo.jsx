58f94b864051ac275544b429f0b4b489f561bbd4
moronq <moronqq@gmail.com>
2022-02-14 23:03:27 +0300

    implemented local-state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,5 +1,8 @@
 import classes from './ProfileInfo.module.css'
 import Preloader from "../../common/Preloader";
+import ProfileStatus from './ProfileStatus'
+import React from "react";
+
 
 const ProfileInfo = (props) => {
 
@@ -8,14 +11,15 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src="https://icdn.lenta.ru/images/2021/04/27/20/20210427205427216/original_7a6a634533af24d3fb1124510487e2be.jpg"
-                    alt="%%%"/>
-            </div>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        src="https://icdn.lenta.ru/images/2021/04/27/20/20210427205427216/original_7a6a634533af24d3fb1124510487e2be.jpg"*/}
+            {/*        alt="%%%"/>*/}
+            {/*</div>*/}
             <div className={classes.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
                 ava + description
+                <ProfileStatus status = {'Hello!'}/>
             </div>
         </div>
     )
