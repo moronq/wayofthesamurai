36054c26ce09dc71cff69b8eca41dd089da33ff6
moronq <moronqq@gmail.com>
2022-03-05 00:21:56 +0300

    implemented some refactoring

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -5,22 +5,17 @@ import React from "react";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 
-const ProfileInfo = (props) => {
+const ProfileInfo = ({profile, status, updateStatus}) => {
 
-    if (!props.profile) {
+    if (!profile) {
         return <Preloader/>
     }
     return (
         <div>
-            {/*<div>*/}
-            {/*    <img*/}
-            {/*        src="https://icdn.lenta.ru/images/2021/04/27/20/20210427205427216/original_7a6a634533af24d3fb1124510487e2be.jpg"*/}
-            {/*        alt="%%%"/>*/}
-            {/*</div>*/}
             <div className={classes.descriptionBlock}>
-                <img src={props.profile.photos.large}/>
+                <img src={profile.photos.large}/>
                 ava + description
-                <ProfileStatusWithHooks status = {props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status = {status} updateStatus={updateStatus}/>
             </div>
         </div>
     )
