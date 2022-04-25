09d540051130d8d0645bcea0eb1145fb6636c156
moronq <moronqq@gmail.com>
2022-04-26 00:49:55 +0300

    change profile reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -8,11 +8,9 @@ const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS'
 
 let initialState = {
     posts: [
-        {id: '1', message: 'Всем привет епа мать', likeCount: '69',},
+        {id: '1', message: 'Всем привет', likeCount: '69',},
         {id: '2', message: 'Эй, почему вы меня не лайкаете???', likeCount: '0',},
         {id: '3', message: 'Салам моим пацанам', likeCount: '14',},
-        {id: '4', message: 'ебать это работает пацаны', likeCount: '14',},
-        {id: '5', message: 'ахуеть я че теперь разраб на реакте???', likeCount: '14',},
     ],
     profile: null,
     status: '',
